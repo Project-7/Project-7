e9605c90f4a71e04c5bee3ec8698f82c10f25ebf
Kalya <Kalya@172.19.223.117>
2014-12-18 13:02:16 +0100

    nuub

---
+++ src/Team.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kalya
+ */
+public class Team {
+
+    private String Team;
+
+    public Team() {
+    }
+
+    public Team(String Team) {
+        this.Team = Team;
+    }
+
+    public String getTeam() {
+        return Team;
+    }
+
+    public void setTeam(String Team) {
+        this.Team = Team;
+    }
+}
