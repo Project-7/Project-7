cd319e3e881c70de03ae80cbc80a60eb3daef14e
Achilles90 <MarkusH@markus-macbook.fritz.box>
2015-02-10 21:24:35 +0100

    changed ERacing and Mitglied

---
+++ ERacing_Main.java
@@ -0,0 +1,55 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author MarkusH
+ */
+public class ERacing_Main {
+
+    /**
+     *
+     */
+    public static void main(String[] args) {
+        Connection con = null;
+        ResultSet rs;
+        Statement stmt;
+        try {
+            System.out.println("Verbindung mit Datenbank wird hergestellt.");
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+            System.err.println("Treiber konnte nicht gefunden werden.");
+            System.err.println(e.getMessage());
+        }
+        try {
+
+            System.out.println("Verbindung ist hergestellt.");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/LAeRacing", "root", "");
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM Mitglied");
+
+            while (rs.next()) {
+                try {
+                    for (int i = 1;; i++) {
+                        System.out.print(rs.getString(i) + "; ");
+                    }
+                } catch (Exception e) {
+                    System.out.println("");
+                }
+            }
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+
+        }
+
+    }
+}
